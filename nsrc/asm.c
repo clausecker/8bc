3bf7ab2e974a28b120dfe8165ff8701d0a9f2ede
Robert Clausecker <fuz@fuz.su>
2019-07-02 22:48:53 +0200

    add asm pretty printer

---
+++ nsrc/asm.c
@@ -0,0 +1,99 @@
+/* asm.c -- PAL pretty printer */
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "asm.h"
+
+FILE *asmfile = NULL;
+
+/*
+ * column tracks where we currently are on the line, with 0 being the
+ * first column on the line.  Each field starts at the column specified
+ * in the enumeration below.  Tabulators are used to advance to the next
+ * field.
+ */
+static unsigned short column = 0;
+enum {
+	FBEGIN = 0,		/* beginning of line */
+	FLABEL = FBEGIN,	/* label field */
+	FINSTR = 8,		/* instruction field */
+	FCOMMENT = 24,		/* commment field */
+};
+
+/*
+ * advance to the indicated column by emitting tabulators.  If we are
+ * already past that column, start a new line.
+ */
+static void
+advance(int target)
+{
+	if (column > target || column == target && target > 0) {
+		fputc('\n', asmfile);
+		column = 0;
+	}
+
+	/* align to a multiple of 8 */
+	column &= ~7;
+
+	while (column <= target) {
+		fputc('\t', asmfile);
+		column += 8;
+	}
+}
+
+extern void
+label(const char *fmt, ...)
+{
+	va_list ap;
+
+	advance(FLABEL);
+
+	va_start(ap, fmt);
+	column += vprintf(fmt, ap);
+	va_end(ap);
+}
+
+extern void
+emit(const char *fmt, ...)
+{
+	va_list ap;
+
+	advance(FINSTR);
+
+	va_start(ap, fmt);
+	column += vprintf(fmt, ap);
+	va_end(ap);
+}
+
+extern void
+comment(const char *fmt, ...)
+{
+	va_list ap;
+
+	advance(FCOMMENT);
+
+	fputc('/', asmfile);
+	fputc(' ', asmfile);
+
+	va_start(ap, fmt);
+	column += vprintf(fmt, ap) + 2;
+	va_end(ap);
+}
+
+extern void
+endline(void)
+{
+	advance(FBEGIN);
+}
+
+extern void
+blank(void)
+{
+	/* do not output multiple blank lines in succession */
+	if (column == 0)
+		return;
+
+	advance(FBEGIN);
+	fputc('\n', asmfile);
+}
---
+++ nsrc/asm.h
@@ -0,0 +1,24 @@
+/*
+ * This module deals with generating formatted assembly output.
+ *
+ * Assembly code is generated in three columns:
+ *
+ *  - label (1 tab stop) contains labels for the current line.
+ *  - instruction (2 tab stops) contains instructions and their
+ *    operands
+ *  - comment (rest of the line) contains comments.
+ *
+ * Text can be written to each of the three columns with the
+ * functions label(), emit(), and comment().  In the case of comment(),
+ * the comment character and a space is implicitly preprended.  A blank
+ * line can be placed in the source file with blank().  Finally, the
+ * current line can be finished with endline().  Assembly output is
+ * written to asmfile which must be set on program startup.  No error
+ * checking is done; it is advised to call ferror(asmfile) at the end to
+ * check for IO errors.
+ */
+extern FILE *asmfile;
+extern void label(const char *fmt, ...);
+extern void emit(const char *fmt, ...);
+extern void comment(const char *fmt, ...);
+extern void blank(void), endline(void);
