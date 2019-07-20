4c83d743d67f943349e20d06ae4c317556e6dee6
Robert Clausecker <fuz@fuz.su>
2019-07-20 03:02:20 +0200

    communicate between the B runtime and the compiler

--- nsrc/main.c
+++ nsrc/main.c
@@ -11,18 +11,43 @@
 #include "name.h"
 #include "parser.h"
 
+/*
+ * Standard library symbol names.  Each name must occur in the
+ * B runtime code and is mapped to a L#### label in main.  Names
+ * are truncated to 6 characters in the runtime code to fit PAL8
+ * restrictions.
+ *
+ * TODO: only do the mapping if the symbol was not redefined by the
+ * program.
+ */
+enum { NSTDLIB = 4 };
+static const char stdlib[NSTDLIB][8] = {
+	"EXIT", "GETCHAR", "PUTCHAR", "SENSE",
+};
+
 extern int
 main(void)
 {
-	struct expr *main;
+	struct expr *e;
+	size_t i;
 
 	asmfile = stdout;
 
 	yyparse();
 	dumpdata();
-	main = define("MAIN");
+
+	/* tell the B runtime where MAIN is */
+	e = define("MAIN");
 	label("MAIN=");
-	emitl(main);
+	emitl(e);
+
+	/* translate library labels to L### labels */
+	for (i = 0; i < NSTDLIB; i++) {
+		e = define(stdlib[i]);
+		setlabel(e);
+		instr("%.6s", stdlib[i]);
+	}
+
 	instr("$");
 	endline();
 
