defc6d27747a0bb354783dd07f340c6748cbb35b
Robert Clausecker <fuz@fuz.su>
2019-08-01 16:22:15 +0200

    rewrite pdp8.c to use state machine
    I'm not happy with the previous approach, so here's a new one: first we
    care about the dirty state and defer the DCA until needed.  Then we give
    the instructions to be generated into a state machine that defers
    emission until an appropriate point of time and remembers invariants
    about the AC state, giving it the ability to perform peephole
    optimisations.  The state machine is written in a coroutine kind of way,
    so it looks a bit uggly, but it should do the trick.  The state machine
    will be in isel() and should come in a future commit.

--- nsrc/pdp8.c
+++ nsrc/pdp8.c
@@ -14,50 +14,66 @@
 /*
  * Machine state.
  *
- * achave
- *     The expression whose value is currently in AC.  If the value
- *     of AC does not correspond to any expression, it is set to
- *     RANDOM.
- *
- * acwant
+ * acstate
  *     The expression whose value should currently be in AC.  If the
  *     value of AC should not correspond to any expression, it is set
  *     to RANDOM.
  *
- * lwant, lhave
- *     The current and desired value of the link bit.  This should be
- *     one of LCLEAR, LSET, LRANDOM.
- *
- * skipstate
- *     The current state of the code generation.  Documented in the
- *     enumeration below.  This is orthogonal to the achave/acwant
- *     mechanism.
- *
  * dirty
- *     This flag is 1 if AC contents need to be deposited into acwant.
- *     If dirty is set, achave == acwant.
+ *     This flag is 1 if dca(acstate) is deferred.  It is set by push(),
+ *     cleared by pop(), writeback(), and acclear() and carefully
+ *     managed by lda().
  */
-/* AC state enumeration */
-enum {
-	CURRENT,  /* nothing deferred (may still need to update AC to acwant) */
-	SKIPABLE, /* next instruction could be skipped, do not alter */
-	SKIPPED,  /* do not emit next instruction */
-};
+static struct expr acstate = { RCONST | 0, "" };
+char dirty = 0;
 
-/* L state enumeration */
-enum {
-	LRANDOM,  /* L bit has unknown value */
-	LCLEAR,   /* L bit is known to be clear */
-	LSET,     /* L bit is known to be set */
-};
-
-static struct expr achave = { RCONST | 0, "" };
-static struct expr acwant = { RCONST | 0, "" };
-char lwant = LRANDOM, lhave = LRANDOM, acstate = CURRENT, dirty = 0;
-
-/* AC value templates */
+/* acstate templates */
 static const struct expr zero = { RCONST | 0, "" };
 static const struct expr random = { RANDOM, "" };
+
+/* forward declarations */
+static void writeback(void);
+static void isel(int, const struct expr *);
+
+extern void
+push(struct expr *e)
+{
+	if (acstate.value != RANDOM && !onstack(acstate.value)) {
+		*e = acstate;
+		return;
+	}
+
+	writeback();
+	emitpush(e);
+	acstate = *e;
+	dirty = 1;
+}
+
+extern void
+pop(struct expr *e)
+{
+	/* omit writeback on immediate pop */
+	if (acstate.value == e->value)
+		dirty = 0;
+
+	emitpop(e);
+}
+
+/*
+ * if dirty is set, write the content of AC to acstate and clear the
+ * dirty flag.
+ */
+static void
+writeback(void)
+{
+	if (dirty) {
+		struct expr e = acstate;
+
+		isel(DCA, &e);
+		dirty = 0;
+		isel(TAD, &e);
+	}
+}
 
 extern struct expr
 r2lval(const struct expr *e)
@@ -90,108 +106,115 @@ l2rval(const struct expr *e)
 extern void
 and(const struct expr *e)
 {
-	catchup();
-	emitand(e);
-	acwant = achave = random;
+	/* strength reduction */
+	if (e->value == (RCONST | 07777)) {
+		isel(NOP, NULL);
+		return;
+	}
+
+	writeback();
+
+	if (e->value == (RCONST | 00000))
+		isel(CLA, NULL);
+	else
+		isel(AND, e);
 }
 
 extern void
 tad(const struct expr *e)
 {
-	catchup();
-	emittad(e);
-	acwant = achave = random;
+	/* strength reduction */
+	if (e->value == (RCONST | 00000)) {
+		isel(NOP, NULL);
+		return;
+	}
+
+	writeback();
+	if (acstate.value != RANDOM && e->value == acstate.value)
+		isel(CLL | RAL, NULL);
+	else
+		isel(TAD, e);
 }
 
 extern void
 isz(const struct expr *e)
 {
-	catchup();
-	emitisz(e);
-	acwant = achave = random;
+	writeback();
+	emitisn(ISZ, e);
 }
 
 extern void
 dca(const struct expr *e)
 {
-	catchup();
-	emitdca(e);
-	acwant = achave = random;
+	writeback();
+	isel(DCA, e);
 }
 
 extern void
 jms(const struct expr *e)
 {
 	catchup();
-	emitjms(e);
-	acwant = achave = random;
+	emitisn(JMS, e);
+	isel(RND, NULL);
 }
 
 extern void
 jmp(const struct expr *e)
 {
 	catchup();
-	emitjmp(e);
+	emitisn(JMP, e);
 }
 
 extern void
 lda(const struct expr *e)
 {
 	/* omit duplicate loads */
-	if (acwant.value == e->value)
+	if (acstate
+.value == e->value)
 		return;
 
-	catchup();
 	opr(CLA);
-	tad(e);
-	acwant = achave = *e;
+	isel(TAD, e);
 }
 
 extern void
 opr(int op)
 {
-	catchup();
-	emitopr(op);
-	acwant = achave = random;
-}
+	if (op != NOP && op != (NOP | OPR2))
+		writeback();
 
-extern void
-push(struct expr *e)
-{
-	if (acwant.value != RANDOM && !onstack(acwant.value)) {
-		*e = acwant;
-		return;
-	}
-
-	emitpush(e);
-	achave = acwant = *e;
-	dirty = 1;
-}
-
-extern void
-pop(struct expr *e)
-{
-	if (acwant.value == e->value)
-		dirty = 0;
-
-	emitpop(e);
+	isel(op, NULL);
 }
 
 extern void
 acclear(void)
 {
-	achave = acwant = zero;
-	lwant = lhave = LRANDOM;
-	acstate = CURRENT;
+	dirty = 0;
+	isel(RST, NULL);
 }
 
 extern void
 catchup(void)
 {
-	if (dirty) {
-		emitdca(&acwant);
-		dirty = 0;
-	}
+	writeback();
+	isel(CUP, NULL);
+}
 
-	/* todo: actually catch up */
+/*
+ *
+ */
+static void
+isel(int op, const struct expr *e)
+{
+	/* dummy */
+	acstate = random;
+	switch (op & 07000) {
+	case CUP:
+	case RST:
+	case RND:
+		break;
+
+	default:
+		emitisn(op, e);
+	}
 }
