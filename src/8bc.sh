7443a8ff04b0808807e32957a6a900c239794278
Robert Clausecker <fuz@fuz.su>
2019-09-02 01:32:26 +0200

    improve 8bc compiler driver

--- src/8bc.sh
+++ src/8bc.sh
@@ -1,22 +1,68 @@
 #!/bin/sh
 
-# PDP-8 B compiler driver
+# B compiler driver
 
-if [ $# -ne 1 ] || [ "${1%.b}" = "$1" ]
+progname="$0"
+bcdir="`dirname $0`"
+bc1="$bcdir/8bc1"
+brt="$bcdir/brt.pal"
+
+usage() {
+	echo Usage: "$progname" [-S] [-o file.bin] file.b	>&2
+	echo " -S  do not assemble"				>&2
+	echo " -o  set output file name"			>&2
+	exit 2
+}
+
+Sflag=
+ofile=
+while getopts So: opt
+do
+	case $opt in
+	S) Sflag=1;;
+	o) ofile="$OPTARG";;
+	?) usage;;
+	esac
+done
+
+shift $((OPTIND-1))
+[ $# -eq 1 ] || usage
+
+stem="${1%.b}"
+if [ "$stem.b" != "$1" ]
 then
-	echo Usage: $0 source.b >&2
-	exit 1
+	echo "$1": B source file required			>&2
+	usage
 fi
 
-palfile="${1%.b}.pal"
-bcdir="`dirname $0`"
-
-exec >"$palfile"
-
-cat "$bcdir/brt.pal"
+exec >"$stem.pal"
+cat "$brt"
 echo
-"$bcdir/8bc1" <"$1" || exit $?
-
+"$bc1" <"$1"
+status=$?
 exec >/dev/null
 
-pal "$palfile" || exit $?
+if [ $status -ne 0 ]
+then
+	rm -f "$stem.pal"
+	exit $status
+fi
+
+if [ ! -z "$Sflag" ]
+then
+	[ ! -z "$ofile" ] && mv "$stem.pal" "$ofile"
+	exit 0
+fi
+
+pal "$stem.pal"
+status=$?
+rm -f "$stem.pal" "$stem.lst"
+
+if [ $status -ne 0 ]
+then
+	rm -f "$stem.bin"
+else
+	[ ! -z "$ofile" ] && mv "$stem.bin" "$ofile"
+fi
+
+exit $status
